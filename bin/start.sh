452f02e113a72c68643ca61b8aea0a432ce2c071
Noah Jahn <36347998+noahjahn@users.noreply.github.com>
2024-01-08 21:16:56 -0500

    Run bun in container (#1)
    * Change start script type to a symlink
    
    * Add bun wrapper shell script and update start script to use it

--- bin/start.sh
+++ bin/start.sh
@@ -2,5 +2,5 @@
 
 docker compose pull || exit 1
 docker compose build || exit 1
-bun install || exit 1
+./bun install || exit 1
 docker compose up || { docker compose down --remove-orphans; exit 0; }
