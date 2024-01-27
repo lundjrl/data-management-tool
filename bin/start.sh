3cdc715b5b93a95e80043b1491ac4b3ed0ebc800
Noah Jahn <najahn19@gmail.com>
2024-01-27 17:05:39 -0500

    Add env validations

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+__file_exists() {
+    test -f "$1"
+}
+
+if ! __file_exists ".env"; then
+    echo "Please create the .env file:"
+    echo "cp .env.example .env"
+    exit 1
+fi
+
 docker compose pull || exit 1
 docker compose build || exit 1
 ./bun install || exit 1
