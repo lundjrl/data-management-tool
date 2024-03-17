9d7a71398ba07375dc9c7a13c3a782a645c0e9bf
lundjrl <jrlbidamin@gmail.com>
2024-03-16 22:59:18 -0400

    Added more DB CRUD endpoints for columns

--- src/routes/database.ts
+++ src/routes/database.ts
@@ -1,15 +1,36 @@
 import { Elysia } from 'elysia'
+import { createBulkColumns, createColumn, deleteBulkColumns, deleteColumn } from '~/services/database/columns'
 import { createTable, deleteTable } from '~/services/database/tables'
 
 const app = new Elysia({ prefix: '/database' })
 
-app.post('/create', async req => {
+app.post('/create/table', async req => {
   const data = await createTable(req.body)
   return { status: 200, data }
 })
 
-app.post('/delete', async req => {
+app.post('/delete/table', async req => {
   const data = await deleteTable(req.body)
+  return { status: 200, data }
+})
+
+app.post('/create/column', async req => {
+  const data = await createColumn(req.body)
+  return { status: 200, data }
+})
+
+app.post('/create/bulk/column', async req => {
+  const data = await createBulkColumns(req.body)
+  return { status: 200, data }
+})
+
+app.post('/delete/column', async req => {
+  const data = await deleteColumn(req.body)
+  return { status: 200, data }
+})
+
+app.post('/delete/bulk/column', async req => {
+  const data = await deleteBulkColumns(req.body)
   return { status: 200, data }
 })
 
--- src/services/database/columns.ts
+++ src/services/database/columns.ts
@@ -1,21 +1,54 @@
 import { prisma } from '../orm/init'
 import { Prisma } from '@prisma/client'
 import { log } from '../logger/log'
+import type { Create_Table } from '~/schemas/forms/Create_Table'
 
 type FN = (tableData: Create_Table) => Promise<boolean>
 
 /**
- * Create a new database table.
+ * Create a new table column.
  * @param tableData
  * @returns boolean
  */
 export const createColumn: FN = async tableData => {
   const { name, columns } = tableData
 
+  const { name: columnName, type } = columns[0]
+
   const query = `
-  CREATE TABLE IF NOT EXISTS ${name} (
+    ALTER TABLE ${name}
+    ADD ${columnName} ${type}
+  `
+
+  log('log', `EXECUTING QUERY: ${query}`)
+
+  const result = await prisma.$queryRaw`${Prisma.raw(query)}`
+
+  return !!result
+}
+
+/**
+ * Create a new table column.
+ * @param tableData
+ * @returns boolean
+ */
+export const createBulkColumns: FN = async tableData => {
+  const { name, columns } = tableData
+
+  let queryString = ''
+
+  columns.forEach((el, index) => {
+    const { name: columnName, type } = el
+    const isLast = index === columns.length - 1
+    const end = isLast ? ';' : ','
+
+    const str = `ADD ${columnName} ${type}${end}`
+    queryString = queryString.concat(str)
+  })
+
+  const query = `
+    ALTER TABLE ${name}
     ${queryString}
-    )
   `
 
   log('log', `EXECUTING QUERY: ${query}`)
@@ -31,9 +64,45 @@ export const createColumn: FN = async tableData => {
  * @returns boolean
  */
 export const deleteColumn: FN = async tableData => {
-  const { name } = tableData
+  const { name, columns } = tableData
 
-  const query = `DROP TABLE IF EXISTS ${name}`
+  const { name: columnName } = columns[0]
+
+  const query = `
+    ALTER TABLE ${name}
+    DROP COLUMN ${columnName}
+    `
+
+  log('log', `EXECUTING QUERY: ${query}`)
+
+  const result = await prisma.$queryRaw`${Prisma.raw(query)}`
+
+  return !!result
+}
+
+/**
+ * Delete a database table.
+ * @param tableData
+ * @returns boolean
+ */
+export const deleteBulkColumns: FN = async tableData => {
+  const { name, columns } = tableData
+
+  let queryString = ''
+
+  columns.forEach((el, index) => {
+    const { name: columnName } = el
+    const isLast = index === columns.length - 1
+    const end = isLast ? ';' : ','
+
+    const str = `DROP COLUMN ${columnName}${end}`
+    queryString = queryString.concat(str)
+  })
+
+  const query = `
+    ALTER TABLE ${name}
+    ${queryString}
+    `
 
   log('log', `EXECUTING QUERY: ${query}`)
 
