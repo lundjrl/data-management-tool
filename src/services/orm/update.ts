9240399ad4a5e1d8a100b0a454f4d8583f6d0d5b
jam <jrlbidamin@gmail.com>
2024-12-29 03:01:24 -0500

    removed git rebase hashes accidentally saved in code

--- src/services/orm/update.ts
+++ src/services/orm/update.ts
@@ -1,6 +1,5 @@
 import { prisma } from './init'
 
-<<<<<<< HEAD
 import type { UpdateOverload } from '~/types/generated/functions'
 
 
@@ -8,13 +7,4 @@ export const update: UpdateOverload = async (key, id, params) => {
   const response = await prisma[key].update({ data: params, where: { id } })
 
   return response as ReturnType<UpdateOverload>
-=======
-import type {UpdateManyOverload} from '~/types/generated/functions'
-
-
-export const update: UpdateOverload = async (key, id, params) => {
-  const response = await prisma[key].update({ data: params, where: { id } })
-
-  return response
->>>>>>> c684e30 (Added type gen for create and update)
 }
