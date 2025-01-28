70b6918609741a7649702ccb86cae486bbded1f4
jam <jrlbidamin@gmail.com>
2025-01-28 08:07:36 -0500

    Ignored prisma model errors in favor of dev database modeling freedom

--- src/services/orm/update.ts
+++ src/services/orm/update.ts
@@ -5,20 +5,24 @@ import { prisma } from './init'
 
 import type { ModelName } from '~/types/ModelName'
 
-type FN = (key: ModelName, id: number, params: object) => Promise<[object, number]>
+type FN = (key: ModelName, id: number, params: object) => Promise<[object, number] | [string | number]>
 
 export const update: FN = async (key, id, params) => {
   try {
     const exists = collectionExists(key)
 
-    if (!exists) { return [`Model ${key} does not exist.`, 400] }
+    if (!exists) {
+      return [`Model ${key} does not exist.`, 400]
+    }
 
+    // @ts-expect-error Loosening up type safety in favor of user DB model freedom.
     const response = await prisma[key].update({ data: params, where: { id } })
 
     return [response, 200]
   }
   catch (error) {
-    log('error', `${error}`)
-    return [error, 400]
+    const message = `${error}`
+    log('error', message)
+    return [message, 400]
   }
 }
--- src/services/orm/updateMany.ts
+++ src/services/orm/updateMany.ts
@@ -5,20 +5,24 @@ import { prisma } from './init'
 
 import type { ModelName } from '~/types/ModelName'
 
-type FN = (key: ModelName, dataParams: object, whereParams: object) => Promise<[object[], number]>
+type FN = (key: ModelName, dataParams: object, whereParams: object) => Promise<[object[], number] | [string, number]>
 
 export const updateMany: FN = async (key, dataParams, whereParams) => {
   try {
     const exists = collectionExists(key)
 
-    if (!exists) { return [`Model ${key} does not exist.`, 400] }
+    if (!exists) {
+      return [`Model ${key} does not exist.`, 400]
+    }
 
+    // @ts-expect-error Loosening up type safety in favor of user DB model freedom.
     const response = await prisma[key].updateMany({ data: dataParams, where: whereParams })
 
     return [response, 200]
   }
   catch (error) {
-    log('error', `${error as string}`)
-    return [error, 400]
+    const message = `${error}`
+    log('error', message)
+    return [message, 400]
   }
 }
